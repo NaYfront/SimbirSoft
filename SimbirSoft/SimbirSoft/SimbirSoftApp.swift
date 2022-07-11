f49837290595bb55c222527d7f9651b0c55c697c
Alexey Gorbunov <nayfront28@gmail.com>
2022-07-11 13:40:35 +0300

    created dev branch

---
+++ SimbirSoft/SimbirSoft/SimbirSoftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimbirSoftApp.swift
+//  SimbirSoft
+//
+//  Created by NaYfront on 11.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SimbirSoftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
