48e4630237ebc883109352b33c929d088dc9eb66
Alexey Gorbunov <nayfront28@gmail.com>
2022-07-14 14:01:52 +0300

    add SwiftLint configuration

---
+++ SimbirSoft/SimbirSoft/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SimbirSoft
+//
+//  Created by NaYfront on 13.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
